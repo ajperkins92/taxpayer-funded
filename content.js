22532bedab4847d6d03921aabdd3a76e5ed9aa8a
Alexander Perkins <alex.jakob.perkins@gmail.com>
2020-03-09 18:34:13 -0700

    Creates the functions for content.js to find and replace words on the window. Also adds images directory for README

--- content.js
+++ content.js
@@ -1,2 +1,27 @@
-// content.js
-alert("Hello from your Chrome extension!");
+var bodyElements = [...document.body.getElementsByTagName('*')];
+// Array of all elements inside the body tag
+
+// Function that loops through the new bodyElements array
+function findAndReplace(){
+  bodyElements.forEach(element =>{
+    element.childNodes.forEach(child =>{
+      if(child.nodeType === 3){
+        replaceText(child);
+      }
+    });
+
+  });
+}
+
+// Function to replace the text within the findAndReplace function
+function replaceText (node) {
+  let value = node.nodeValue;
+  value = value.replace(/Free/gi, 'Taxpayer-funded');
+  value = value.replace(/free/gi, 'taxpayer-funded');
+  value = value.replace(/Bernie/gi, 'Comrade');
+  value = value.replace(/socialism/gi, 'wealth confiscation');
+  value = value.replace(/Socialism/gi, 'Wealth confiscation');
+  node.nodeValue = value;
+}
+
+window.onload = findAndReplace();
